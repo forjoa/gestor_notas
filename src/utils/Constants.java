65946f0c86b482dc6fd121b9ee696f4aae7deee1
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-20 19:51:14 +0100

    build: update app color and add user and password

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -6,8 +6,10 @@ import java.awt.Font;
 public class Constants {
 
     public static final String APP_NAME = "GESTOR DE NOTAS";
-    public static final Color APP_COLOR = new Color(0, 102, 204);
+    public static final Color APP_COLOR = new Color(169, 56, 183);
     public static final Font APP_FONT_TITLE = new Font("Inter", Font.BOLD, 24);
     public static final Font APP_FONT_TEXT = new Font("Inter", Font.BOLD, 14);
+    public static final String USER = "admin";
+    public static final String PASSWORD = "1234";
 
 }
