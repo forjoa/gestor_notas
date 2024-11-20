bda9675f09228b43d9f5abaa50717fec4e186c17
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-20 19:36:46 +0100

    build: file for static constants

---
+++ src/utils/Constants.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class Constants {
+
+    public static final String APP_NAME = "GESTOR DE NOTAS";
+    public static final Color APP_COLOR = new Color(0, 102, 204);
+    public static final Font APP_FONT_TITLE = new Font("Inter", Font.BOLD, 24);
+    public static final Font APP_FONT_TEXT = new Font("Inter", Font.BOLD, 14);
+
+}
