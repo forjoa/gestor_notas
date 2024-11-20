299438beb750834a2d03be79ab6b8e0404759806
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-20 19:59:05 +0100

    refactor: update font-family

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -7,8 +7,8 @@ public class Constants {
 
     public static final String APP_NAME = "GESTOR DE NOTAS";
     public static final Color APP_COLOR = new Color(169, 56, 183);
-    public static final Font APP_FONT_TITLE = new Font("Inter", Font.BOLD, 24);
-    public static final Font APP_FONT_TEXT = new Font("Inter", Font.BOLD, 14);
+    public static final Font APP_FONT_TITLE = new Font("JetBrains Mono", Font.BOLD, 24);
+    public static final Font APP_FONT_TEXT = new Font("JetBrains Mono", Font.BOLD, 14);
     public static final String USER = "admin";
     public static final String PASSWORD = "1234";
 
