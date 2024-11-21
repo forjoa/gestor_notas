4bae982767ecebdff364f70f405570a238bf6d62
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-21 19:49:05 +0100

    feat: add note by title

--- src/app/Manager.java
+++ src/app/Manager.java
@@ -5,9 +5,12 @@ import java.util.Objects;
 import javax.swing.*;
 
 import models.Note;
+import models.NoteCellRenderer;
 
 public class Manager extends JPanel {
 
+    private final DefaultListModel<Note> noteModel;
+    private final JList<Note> noteList;
     private final String[] lookAndFeels = {
             "Metal (Cross-Platform)",
             "Windows",
@@ -17,6 +20,11 @@ public class Manager extends JPanel {
     };
 
     public Manager() {
+        noteModel = new DefaultListModel<>();
+        noteList = new JList<>(noteModel);
+        noteList.setCellRenderer(new NoteCellRenderer());
+        noteList.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         JPanel panelUI = new JPanel();
         panelUI.setLayout(new FlowLayout(4));
@@ -25,15 +33,48 @@ public class Manager extends JPanel {
         add(lafc);
 
         JPanel contentPanel = new JPanel();
-        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
-        contentPanel.add(new JList<Note>());
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
+        contentPanel.add(noteList);
         contentPanel.add(new JTextArea());
         add(contentPanel);
 
         JPanel optionsPanel = new JPanel();
         optionsPanel.setLayout(new FlowLayout());
-        optionsPanel.add(new JButton("Agregar nota"));
-        optionsPanel.add(new JButton("Eliminar nota"));
+        JButton addNoteButton = new JButton("Agregar nota");
+        addNoteButton.addActionListener(e -> {
+            JDialog dialogSetTitleNote = new JDialog();
+            dialogSetTitleNote.setModal(true);
+            dialogSetTitleNote.setTitle("Agregar nota");
+            dialogSetTitleNote.setSize(300, 100);
+            dialogSetTitleNote.setLocationRelativeTo(null);
+            Note note = new Note();
+            JPanel panel = new JPanel();
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(new JLabel("Titulo"));
+            panel.add(new JTextField());
+            JButton bAccept = new JButton("Aceptar");
+            bAccept.addActionListener(e1 -> {
+                note.setTitle(((JTextField) panel.getComponent(1)).getText());
+                dialogSetTitleNote.dispose();
+            });
+            panel.add(bAccept);
+            dialogSetTitleNote.add(panel);
+
+            dialogSetTitleNote.setVisible(true);
+
+            noteModel.addElement(note);
+        });
+        optionsPanel.add(addNoteButton);
+
+        JButton deleteNoteButton = new JButton("Eliminar nota");
+        deleteNoteButton.addActionListener(e -> {
+            int selected = noteList.getSelectedIndex();
+            if (selected != -1) {
+                noteModel.remove(selected);
+            }
+        });
+        optionsPanel.add(deleteNoteButton);
+
         add(optionsPanel);
 
         add(panelUI);
